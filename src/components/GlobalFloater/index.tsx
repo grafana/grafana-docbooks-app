0903b693e45dd92e4fff24a03d9203f393e014fe
Darren Janeczek <darren.janeczek@grafana.com>
2024-03-01 20:21:13 -0500

    the battle rages on

--- src/components/GlobalFloater/index.tsx
+++ src/components/GlobalFloater/index.tsx
@@ -1,24 +1,39 @@
 import { css } from "@emotion/css";
 import { GrafanaTheme2 } from "@grafana/data";
-import { Drawer, IconButton, useStyles2, useTheme2 } from "@grafana/ui";
-import React, { useCallback, useEffect, useState } from "react";
+import { locationService } from "@grafana/runtime";
+import { IconButton, useStyles2 } from "@grafana/ui";
+import React, { CSSProperties, useCallback, useEffect, useState } from "react";
+
+import history from 'history'
 
 import ReactDOM from 'react-dom/client'
 
 type Props = {};
 
+const DONT_MOVE_BUTTON = false;
+
+const firstLocation = locationService.getHistory().location;
+
 export function GlobalFloater(props: Props) {
 
   const styles = useStyles2(getStyles);
   const [mouseDown, setMouseDown] = useState(false);
-  // const [position, setPosition] = useState([])
+  const [position, setPosition] = useState<[number, number]>()
   const [isDrawerOpen, setDrawerOpen] = useState(false);
 
-  // const [activities, setActivities] = useState<Activity>([]);
+  const [locations, setLocations] = useState<history.Update[]>([]);
 
-  const openDrawer = useCallback(()=>setDrawerOpen(true), [setDrawerOpen]);
-  const closeDrawer = useCallback(()=>setDrawerOpen(false), [setDrawerOpen]);
-  
+  const toggleDrawer = useCallback(()=>setDrawerOpen(!isDrawerOpen), [setDrawerOpen, isDrawerOpen]);
+
+  useEffect(()=>{
+    const unsubscribe = locationService.getHistory().listen((update: history.Update)=>{
+      setLocations([...locations, update]);
+    })
+
+    return unsubscribe;
+
+  }, [locations, setLocations])
+
   useEffect(()=>{
 
     // Press d followed by b to open docbooks
@@ -28,14 +43,18 @@ export function GlobalFloater(props: Props) {
 
     const handleKeyDown = (e: KeyboardEvent) => {
 
-      if (e.target !== document.body) {
-        return;
+      if (e.target instanceof HTMLElement) {
+        if (e.target.tagName === 'input') {
+          console.log("REJECTED TARGET", e.target)
+          return;
+        }
       }
+
 
       const deltaTime = e.timeStamp - prevTime;
 
       if (prevKey === 'd' && e.key === 'b' && deltaTime < 1000) {
-        openDrawer();
+        setDrawerOpen(true);
       }
 
       prevKey = e.key;
@@ -48,25 +67,63 @@ export function GlobalFloater(props: Props) {
         document.removeEventListener('keydown', handleKeyDown);
       };
 
-  }, [openDrawer]);
+  }, [setDrawerOpen]);
   
   useEffect(()=>{
     console.log("MOUSE IS DOWN", mouseDown)
+
+    
+
+    if (mouseDown && !DONT_MOVE_BUTTON) {
+      function mouseHandler(e: MouseEvent) {
+        console.log(e);
+        setPosition([e.pageX, e.pageY]);
+      }
+      document.addEventListener('mousemove', mouseHandler)
+
+      return ()=>document.removeEventListener('mousemove', mouseHandler);
+    }
+
+    return;
+
   }, [mouseDown])
 
-  return <div onMouseDown={()=>setMouseDown(true)} onMouseUp={()=>setMouseDown(false)}>
-    {isDrawerOpen && <Drawer onClose={closeDrawer} title='Team name here' subtitle='Run books' closeOnMaskClick>
-        Hello, thank you for being here.
-        <div>
-          Do we want this to be a modal blocker?
-        </div>
-        <div>
-          <h1>Events</h1>
 
-        </div>
-      </Drawer>
-    }
-    <IconButton className={styles.floater} aria-label="Open run book" size="xxxl" tooltip={'Run books'} variant="primary" name={'book-open'} onClick={openDrawer}>Run Books</IconButton>
+
+
+  const styleOverride: CSSProperties = isDrawerOpen ? {} : {width: 0}
+
+  const positionOverride: CSSProperties = {};
+
+  if (position) {
+    let [x, y] = position;
+    positionOverride.left = x;
+    positionOverride.top = y;
+    positionOverride.bottom = 'revert';
+    positionOverride.right = 'revert';
+  }
+
+  return <div>
+    <div className={styles.drawer} style={styleOverride}>
+      <div className={styles.drawerContents}>
+
+        <h1>Team name here</h1>
+        Doc books
+          
+        <h2>Your first location</h2>
+        <ul>
+          <li>{JSON.stringify(firstLocation)}</li>
+        </ul>
+
+        <h2>Where else have you been?</h2>
+        <ul>
+          {locations.map((update, i) => <li key={i} className={styles.locationItem}>{JSON.stringify(update)}</li>)}
+        </ul>
+
+      </div>
+    </div>
+    
+    <IconButton style={positionOverride} onMouseDown={(e)=>e.button === 0 && setMouseDown(true)} onMouseUp={(e)=> e.button === 0 && setMouseDown(false)} className={styles.floater} aria-label="Open run book" size="xxxl" tooltip={'Run books'} variant="primary" name={'book-open'} onClick={toggleDrawer}>Run Books</IconButton>
   </div>
 
 
@@ -74,7 +131,24 @@ export function GlobalFloater(props: Props) {
 
 
 function getStyles(theme: GrafanaTheme2) {
+
+  const drawerWidth = theme.spacing(64);
+
   return ({
+    drawer: css({
+      width: drawerWidth,
+      height: '100%',
+      transition: "width 0.2s ease-in-out",
+      // background: theme.colors.background.primary,
+      // borderLeft: `solid 1px ${theme.colors.border.strong}`,
+    }),
+    locationItem: css({
+      ... theme.typography.bodySmall
+    }),
+    drawerContents: css({
+      width: drawerWidth,
+      margin: theme.spacing(1),
+    }),
     floater: css({
       borderRadius: '100%',
       // background: theme.colors.info.main,
@@ -99,7 +173,15 @@ function getStyles(theme: GrafanaTheme2) {
 
 export function setUpGlobalFloater() {
   const floater = document.createElement('div');
+  const pageContent = document.getElementById('pageContent');
+
+  if (pageContent === null) {
+    console.log("COulnd't find it");
+    setTimeout(setUpGlobalFloater, 100);
+  }
+
+
   floater.id = 'grafana_docbooks_app_floater';
-  document.body.appendChild(floater);
+  pageContent?.parentElement?.appendChild(floater);
   ReactDOM.createRoot(floater).render(<GlobalFloater />)
 }
